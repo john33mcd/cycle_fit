9dcd735eee92b3da63be1802e1aebf4bd20d40b8
john33mcd <jmcd-34@hotmail.com>
2023-08-12 15:32:08 +0000

    added checkout view and fixed noimage code on each page

--- cycle_fit/settings.py
+++ cycle_fit/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'subscriptions',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cycle_fit.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
