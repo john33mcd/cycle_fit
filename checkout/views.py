9e23c8d7c6321e89f9fcc35b6132ff54993c01f1
john33mcd <jmcd-34@hotmail.com>
2023-08-13 11:53:00 +0000

    added stripe elements payment section

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NecXAEZyfC3GoAQK8eYMhjhGR66z21RLAcuJC2wWc21FGhWcalTsBUVvrOymSwo94ilexVpntvn02DBoQoNzUdW00u7U6Bd54',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
